72fbd8433d421a8dc7b6345a79abcb96b4594b1b
Judith Mary <judithharipraksh@gmail.com>
2015-06-30 15:25:09 +0000

    Jinja template used for html rendering

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from pygenome import app
+from flask import render_template
 
 @app.route('/')
 def hello():
-	return "Peedu loves Judo!!"
-
+	return render_template("index.html", message="Peedu loves Judo !!!. Message to my love")
